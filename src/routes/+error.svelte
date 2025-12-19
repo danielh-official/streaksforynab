beb83f0caf857a5e403ed40ba592db2d82d6b591
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-19 18:40:06 -0500

    feat: add error page component for handling application errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<svelte:head>
+	<title>Error | Streaks (For YNAB)</title>
+</svelte:head>
+
+<div class="min-h-screen flex flex-col justify-center items-center gap-y-6">
+  <h1 class="text-xl mb-2">{page.status}</h1>
+  <p class="text-gray-400 text-lg">{page?.error?.message ?? 'An unexpected error occurred.'}</p>
+  <a class="mt-5 text-blue-500 hover:underline" href="/">Go back to the homepage</a>
+</div>
