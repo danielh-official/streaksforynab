8f36e50fe86d5684c01850380b292fa89170ce26
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-18 20:01:54 -0500

    Implement ynab api auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+	import { derived } from 'svelte/store';
 
 	let loading = $state(false);
 
@@ -15,6 +16,28 @@
 
 		loading = false;
 	});
+
+	let currentUrl = derived([], () => {
+		if (browser) {
+			return window.location.href;
+		}
+		return '';
+	});
+
+	let authUrl = derived(currentUrl, ($currentUrl) => {
+		const clientId = 'BcdLFTpW1QxdDNy0RwfCiuTxEKSYMB0i3cQRB8SpkeY';
+
+		const redirectUri = (`${$currentUrl}callback`);
+
+		return `https://app.ynab.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token`;
+	});
+
+	let authToken = derived([], () => {
+		if (browser) {
+			return sessionStorage.getItem('ynab_access_token') || null;
+		}
+		return null;
+	});
 </script>
 
 <svelte:head>
@@ -22,12 +45,28 @@
 	<meta name="description" content="A web app to track your spending habits with YNAB." />
 </svelte:head>
 
-{#if loading}
-	<div class="flex justify-center items-center h-screen">
+<div class="flex justify-center items-center h-screen">
+	{#if loading}
 		<div
 			class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"
 		></div>
-	</div>
-{:else}
-	<div></div>
-{/if}
+	{:else}
+		{#if $authToken}
+			<div class="text-center">
+				<h1 class="text-2xl font-bold mb-4">You are logged in!</h1>
+
+				
+			</div>
+		{:else}
+			<div class="text-center">
+				<h1 class="text-2xl font-bold mb-4">Welcome to Streaks (For YNAB)</h1>
+				<p class="mb-4">Please log in with your YNAB account to continue.</p>
+				<a
+					href="{$authUrl}"
+					class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+					>Log in with YNAB</a
+				>
+			</div>
+		{/if}
+	{/if}
+</div>
---
+++ src/routes/callback/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		// Get hash access token from URL
+
+		const hash = window.location.hash.substring(1);
+		const params = new URLSearchParams(hash);
+		const accessToken = params.get('access_token');
+
+		if (accessToken) {
+			// Store the access token securely (e.g., in localStorage or a Svelte store)
+			sessionStorage.setItem('ynab_access_token', accessToken);
+
+			// Redirect to the main page or dashboard
+			window.location.href = '/';
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>Streaks (For YNAB)</title>
+	<meta name="description" content="A web app to track your spending habits with YNAB." />
+</svelte:head>
